4eddd3ea28f32e343f30c7a97034bde15a7e5f2c
Dougie Dunn <dougie.dunn@gmail.com>
2022-04-19 11:50:05 +0000

    Updated resource group name

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/IntergalacticExpress/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "myresourcegroup.name"
+  resource_group_name = "myresourcegroup"
 }
